c8aa91f04cf1f50ece969bc9b8ba56655388577d
BensonGitau <bensonkamaugitau@gmail.com>
2024-03-18 21:41:32 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+const employee = {
+    name: "Benny",
+    address: "Nairobi"
+};
+console.log(employee);
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value,
+    };
+    
+}
+console.log(employee);
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+console.log(employee);
+
+function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+    
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    
+    delete employee[key];
+    
+    return employee;
+}
