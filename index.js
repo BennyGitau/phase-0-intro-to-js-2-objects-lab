3c5f3e8527f9077fa22158f4148a542349fd3ba1
BensonGitau <bensonkamaugitau@gmail.com>
2024-03-21 18:47:12 +0300

    Complete lab

--- index.js
+++ index.js
@@ -3,33 +3,35 @@ const employee = {
     name: "Benny",
     address: "Nairobi"
 };
-console.log(employee);
 
+//returns an employee with the original key value pairs and the new key value pair
+//it does not modify the original employee, but rather returns a clone with the new data
 function updateEmployeeWithKeyAndValue(employee, key, value) {
     return {
         ...employee,
-        [key]: value,
-    };
-    
+        [key]:value,
+    }
 }
-console.log(employee);
 
+//updates `employee` with the given `key` and `value` (it is destructive) and returns the entire updated employee
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
     employee[key] = value;
     return employee;
 }
-console.log(employee);
+
+//deletes `key` from a clone of employee and returns the new employee (it is non-destructive)
+//does not modify the original employee (it is non-destructive)
 
 function deleteFromEmployeeByKey(employee, key) {
-    let newEmployee = { ...employee };
-    delete newEmployee[key];
-    return newEmployee;
+        const newEmployee = { ...employee};
+        delete newEmployee[key];
+        return newEmployee;
 }
 
-    
+
+//returns employee without the deleted key/value pair
+//modifies the original employee
 function destructivelyDeleteFromEmployeeByKey(employee, key) {
-    
     delete employee[key];
-    
     return employee;
 }
